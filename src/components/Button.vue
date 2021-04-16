0ed4f91dfa2cd1ee93027266944726421650fa2b
rnair <ratishnair21@gmail.com>
2021-04-16 22:21:55 +0400

    A simple task tracker using VueJS

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :style="{ background: color }" @click="onClick()" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("btn-click")
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    {{ title }}
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button"
+
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  emits: ["toggle-add-task"],
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
